7102e4a07940775b0e676e2fa837fa76ba469a67
repeckadonatas <repeckadonatas@yahoo.comm>
2024-09-28 21:31:48 +0300

    Updated SQL for the current project database

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
 CREATE ROLE root;
 ALTER ROLE root WITH LOGIN;
 CREATE DATABASE "root";
-CREATE DATABASE "jobs_data_db";
+CREATE DATABASE "sales-data-db";
 
 CREATE SCHEMA IF NOT EXISTS staging;
 
@@ -10,14 +10,14 @@ CREATE EXTENSION IF NOT EXISTS dblink;
 DO
 $do$
 BEGIN
-   IF EXISTS (SELECT FROM pg_database WHERE datname = 'jobs_data_db') THEN
+   IF EXISTS (SELECT FROM pg_database WHERE datname = 'sales-data-db') THEN
       RAISE NOTICE 'Database already exists';
    ELSE
       PERFORM dblink_exec('dbname=' || current_database()
-                        , 'CREATE DATABASE "jobs_data_db"');
+                        , 'CREATE DATABASE "sales-data-db"');
    END IF;
 END
 $do$;
 
 CREATE USER airflow WITH PASSWORD 'airflow';
-GRANT ALL PRIVILEGES ON DATABASE jobs_data_db TO airflow;
+GRANT ALL PRIVILEGES ON DATABASE sales_data_db TO airflow;
