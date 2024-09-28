b35dba5114d2ac4adc1bd2c590c5eb153a9ad0a1
repeckadonatas <repeckadonatas@yahoo.comm>
2024-09-28 22:59:38 +0300

    Updated the init script

--- sql/init.sql
+++ sql/init.sql
@@ -1,23 +1,26 @@
 CREATE ROLE root;
 ALTER ROLE root WITH LOGIN;
 CREATE DATABASE "root";
-CREATE DATABASE "sales-data-db";
+CREATE DATABASE "sales_data_db";
 
-CREATE SCHEMA IF NOT EXISTS staging;
+CREATE ROLE db_admin;
+GRANT ALL PRIVILEGES ON DATABASE "sales_data_db" TO db_admin;
+
+ALTER ROLE db_admin WITH PASSWORD :'postgres_password';
 
 CREATE EXTENSION IF NOT EXISTS dblink;
 
 DO
 $do$
 BEGIN
-   IF EXISTS (SELECT FROM pg_database WHERE datname = 'sales-data-db') THEN
+   IF EXISTS (SELECT FROM pg_database WHERE datname = 'sales_data_db') THEN
       RAISE NOTICE 'Database already exists';
    ELSE
       PERFORM dblink_exec('dbname=' || current_database()
-                        , 'CREATE DATABASE "sales-data-db"');
+                        , 'CREATE DATABASE "sales_data_db"');
    END IF;
 END
 $do$;
 
 CREATE USER airflow WITH PASSWORD 'airflow';
-GRANT ALL PRIVILEGES ON DATABASE sales_data_db TO airflow;
+GRANT ALL PRIVILEGES ON DATABASE "sales_data_db" TO airflow;
