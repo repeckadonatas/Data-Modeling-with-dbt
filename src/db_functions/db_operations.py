4cd593fbfa3dc94d23efa8d661cd20df5458bcd9
repeckadonatas <repeckadonatas@yahoo.comm>
2024-09-30 20:00:15 +0300

    Updated functions. Improved log output messages

--- src/db_functions/db_operations.py
+++ src/db_functions/db_operations.py
@@ -1,5 +1,4 @@
 import pyspark
-import sqlalchemy
 from sqlalchemy import inspect
 from sqlalchemy.exc import SQLAlchemyError, OperationalError, DBAPIError, DatabaseError, DisconnectionError
 
@@ -7,27 +6,20 @@ import src.db_functions.db_tables as db_tables
 from src.db_functions.db_connection import db_logger, DatabaseConnection
 
 
-# class DataUpload(JobsDataDatabase):
-#     """
-#     Functions that determine the logic of data upload process.
-#     """
-#
-#     def __init__(self):
-#         super().__init__()
-
-def create_tables(engine: DatabaseConnection) -> None:
+def create_tables(db_conn: DatabaseConnection) -> None:
     """
     Creates tables in a database if they do not exist.
-    :param engine: SQLAlchemy database engine.
+    :param db_conn: SQLAlchemy database engine.
     """
     try:
-        inspector = inspect(engine.engine)
+        engine = db_conn.engine
+        inspector = inspect(engine)
 
         for table in db_tables.Base.metadata.tables.values():
             table_name = table.name
 
             if not inspector.has_table(table_name):
-                with engine.engine.begin() as connection:
+                with engine.begin() as connection:
                     table.create(connection)
                 db_logger.info('Table "%s" created successfully!', table_name)
             else:
@@ -37,46 +29,51 @@ def create_tables(engine: DatabaseConnection) -> None:
     except (SQLAlchemyError, OperationalError, DatabaseError, DisconnectionError, DBAPIError, AttributeError) as e:
         db_logger.error('An error occurred while creating tables: %s', e, exc_info=True)
 
-def get_tables_in_db(engine: DatabaseConnection) -> list:
+
+def get_tables_in_db(db_conn: DatabaseConnection) -> list:
     """
     Returns a list of all the tables in the database.
-    :param engine: SQLAlchemy database engine.
+    :param db_conn: SQLAlchemy database engine.
     :return: List of tables in the database.
     """
-    inspect_db = inspect(engine.engine)
-    tables_list = []
+    engine = db_conn.engine
+    inspect_db = inspect(engine)
+    get_tables = inspect_db.get_table_names()
 
-    for table in inspect_db.get_table_names():
-        tables_list.append(table)
+    return get_tables
 
-    return tables_list
 
-def load_to_database(engine: DatabaseConnection,
+def load_to_database(db_conn: DatabaseConnection,
                      dataframe: pyspark.sql.DataFrame,
                      table_name: str) -> None:
     """
     Function to load the data of a dataframe to a specified table in the database.
-    :param engine: SQLAlchemy database engine.
+    :param db_conn: SQLAlchemy database engine.
     :param dataframe: dataframe to load data from.
     :param table_name: table to load the data to.
     """
     try:
+        engine = db_conn.engine
         dataframe.to_sql(table_name,
-                         con=engine.engine,
+                         con=engine,
                          if_exists='replace',
                          index=False)
 
     except (SQLAlchemyError, OperationalError, DatabaseError, DisconnectionError, DBAPIError, AttributeError) as e:
         db_logger.error("An error occurred while loading the data: %s. "
                         "Rolling back the last transaction", e, exc_info=True)
-        engine.engine.rollback()
+        engine.rollback()
 
 
 if __name__ == '__main__':
 
     try:
         with DatabaseConnection() as dc:
-            create_tables(dc)
-            get_tables_in_db(dc)
+            if dc is not None:
+                create_tables(dc)
+                tables = get_tables_in_db(dc)
+                table_count = len(tables)
+                db_logger.info(f'\nFound %s table{"s" if table_count != 1 else ""} in a database:\n'
+                               '%s', table_count, tables)
     except SQLAlchemyError as e:
         db_logger.error('An error occurred while creating tables: %s', e, exc_info=True)
