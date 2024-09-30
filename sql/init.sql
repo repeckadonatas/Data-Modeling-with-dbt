1af4c19c18240a95f960416add90aab135b90e3d
repeckadonatas <repeckadonatas@yahoo.comm>
2024-09-30 15:56:09 +0300

    init.sql update. Should work with environment variables set in .env when creating role and db

--- sql/init.sql
+++ sql/init.sql
@@ -1,26 +1,34 @@
 CREATE ROLE root;
 ALTER ROLE root WITH LOGIN;
 CREATE DATABASE "root";
-CREATE DATABASE "sales_data_db";
-
-CREATE ROLE db_admin;
-GRANT ALL PRIVILEGES ON DATABASE "sales_data_db" TO db_admin;
-
-ALTER ROLE db_admin WITH PASSWORD :'postgres_password';
 
 CREATE EXTENSION IF NOT EXISTS dblink;
 
 DO
 $do$
 BEGIN
-   IF EXISTS (SELECT FROM pg_database WHERE datname = 'sales_data_db') THEN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${PGUSER}') THEN
+      CREATE ROLE ${PGUSER} WITH LOGIN PASSWORD ${PGPASSWORD};
+   END IF;
+
+   IF EXISTS (SELECT FROM pg_database WHERE datname = '${PGDATABASE}') THEN
       RAISE NOTICE 'Database already exists';
    ELSE
       PERFORM dblink_exec('dbname=' || current_database()
-                        , 'CREATE DATABASE "sales_data_db"');
+                        , 'CREATE DATABASE "${PGDATABASE}"');
    END IF;
 END
 $do$;
 
-CREATE USER airflow WITH PASSWORD 'airflow';
-GRANT ALL PRIVILEGES ON DATABASE "sales_data_db" TO airflow;
+\c ${PGDATABASE}
+
+GRANT ALL PRIVILEGES ON DATABASE ${PGDATABASE} TO ${PGUSER};
+
+-- CREATE USER airflow WITH PASSWORD 'airflow';
+-- GRANT ALL PRIVILEGES ON DATABASE "sales_data_db" TO airflow;
+
+-- CREATE ROLE db_admin;
+-- ALTER ROLE db_admin WITH LOGIN;
+-- CREATE USER db_admin WITH PASSWORD :'postgres_password';
+-- ALTER ROLE db_admin WITH PASSWORD :'postgres_password';
+--
