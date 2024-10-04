1e08503df232da8f349c913cf729b28181bec85a
repeckadonatas <repeckadonatas@yahoo.comm>
2024-10-04 13:55:40 +0300

    Updated entrypoint shell commands

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 SPARK_WORKLOAD=$1
 
 echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
@@ -7,10 +9,25 @@ echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
   start-master.sh -p 7077
+
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
-  start-worker.sh spark://spark-master:7077
-#elif [ "$SPARK_WORKLOAD" == "history" ]
-#then
-#  start-history-server.sh
+  WORKER_PORT=${2:-8081}
+  echo "$WORKER_PORT"
+
+  start-worker.sh spark://spark-master:7077 --webui-port "$WORKER_PORT"
 fi
+
+#case $SPARK_WORKLOAD in
+#  master)
+#    exec start-master.sh -p 7077
+#    ;;
+#  worker)
+#    exec start-worker.sh spark://spark-master:7077
+#    ;;
+#  *)
+#    echo "Invalid SPARK_WORKLOAD: $SPARK_WORKLOAD"
+#    echo "Usage: $0 {master|worker}"
+#    exit 1
+#    ;;
+#esac
