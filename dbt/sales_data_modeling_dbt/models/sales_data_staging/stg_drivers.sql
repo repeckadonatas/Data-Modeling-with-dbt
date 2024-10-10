fe158d9483ae0c9cfa4be1da67ed3f10db4f8015
repeckadonatas <repeckadonatas@yahoo.comm>
2024-10-10 21:32:02 +0300

    dbt model for drivers view

--- dbt/sales_data_modeling_dbt/models/sales_data_staging/stg_drivers.sql
+++ dbt/sales_data_modeling_dbt/models/sales_data_staging/stg_drivers.sql
@@ -1,7 +1,16 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-select
+    select
 
-    *
+        driver_id,
+        driver_ref,
+        driver_number,
+        driver_code,
+        first_name,
+        last_name,
+        CAST(date_of_birth as DATE) as date_of_birth,
+        driver_nationality,
+        driver_url,
+        CAST(timestamp as TIMESTAMP WITH TIME ZONE) as timestamp
 
-from public.drivers
+    from {{ source('raw_f1_data', 'drivers') }}
