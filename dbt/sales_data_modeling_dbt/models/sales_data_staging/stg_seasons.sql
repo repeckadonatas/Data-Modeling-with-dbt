ab6ffb6481f14b262279aab50a41e8e900437da7
repeckadonatas <repeckadonatas@yahoo.comm>
2024-10-13 16:55:38 +0300

    Fixed column selection

--- dbt/sales_data_modeling_dbt/models/sales_data_staging/stg_seasons.sql
+++ dbt/sales_data_modeling_dbt/models/sales_data_staging/stg_seasons.sql
@@ -3,7 +3,7 @@
     select
 
         CAST(year as INT) as year,
-        url as season_url,
+        season_url,
         CAST(timestamp as TIMESTAMP WITH TIME ZONE) as timestamp
 
     from {{ source('raw_f1_data', 'seasons') }}
