18b29425a5c0d9832d0518d12f3b081ceeacc813
repeckadonatas <repeckadonatas@yahoo.comm>
2024-10-16 22:24:13 +0300

    Removing nonexisting folders/files

--- dbt/sales_data_modeling_dbt/models/sales_data_staging/stg_seasons.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-    select
-
-        CAST(year as INT) as year,
-        season_url,
-        CAST(timestamp as TIMESTAMP WITH TIME ZONE) as timestamp
-
-    from {{ source('raw_f1_data', 'seasons') }}
--- dbt/sales_data_modeling_dbt/models/sales_data_staging/stg_status.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-    select
-
-        status_id,
-        status,
-        CAST(timestamp as TIMESTAMP WITH TIME ZONE) as timestamp
-
-    from {{ source('raw_f1_data', 'status') }}
